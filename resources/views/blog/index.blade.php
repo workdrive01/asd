7737d4716b7bea052c74959439c9d0d126e7c823
Jugal <workdrive0110@gmail.com>
2019-07-23 15:16:10 +0545

    using faker added faker data in post and user

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,51 +2,51 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
 
-            @foreach($posts as $post)
+                @foreach($posts as $post)
 
-                <article class="post-item">
-                <div class="post-item-image">
-                    <a href="post.html">
-                        <img src="{{ asset('img/{{ $post->image }}') }}" alt="">
-                    </a>
-                </div>
-                <div class="post-item-body">
-                    <div class="padding-10">
-                        <h2><a href="post.html">{{ $post->title }}</a></h2>
-                        <p>{{ $post -> exerpt }}</p>
-                    </div>
-
-                    <div class="post-meta padding-10 clearfix">
-                        <div class="pull-left">
-                            <ul class="post-meta-group">
-                                <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
-                                <li><i class="fa fa-clock-o"></i><time> February 12, 2016</time></li>
-                                <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
-                                <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
-                            </ul>
+                    <article class="post-item">
+                        <div class="post-item-image">
+                            <a href="post.html">
+                                <img src="/img/{{ $post->image }}'" alt="">
+                            </a>
                         </div>
-                        <div class="pull-right">
-                            <a href="post.html">Continue Reading &raquo;</a>
-                        </div>
-                    </div>
-                </div>
-            </article>
+                        <div class="post-item-body">
+                            <div class="padding-10">
+                                <h2><a href="post.html">{{ $post->title }}</a></h2>
+                                <p>{{ $post -> exerpt }}</p>
+                            </div>
 
-            @endforeach
-            <nav>
-                <ul class="pager">
-                    <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
-                    <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
-                </ul>
-            </nav>
+                            <div class="post-meta padding-10 clearfix">
+                                <div class="pull-left">
+                                    <ul class="post-meta-group">
+                                        <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
+                                        <li><i class="fa fa-clock-o"></i>
+                                            <time> February 12, 2016</time>
+                                        </li>
+                                        <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
+                                        <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
+                                    </ul>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="post.html">Continue Reading &raquo;</a>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                @endforeach
+                <nav>
+                    <ul class="pager">
+                        <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+                        <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+            @include('layouts.sidebar')
         </div>
-
-        @include('layouts.sidebar')
     </div>
-</div>
-    @endsection
+@endsection
 
