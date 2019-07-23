0e463bf0fd057d695ac1df6f5d9c41afb5fa27eb
Jugal <workdrive0110@gmail.com>
2019-07-23 14:24:45 +0545

    using faker added faker data in post and user

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    public function index(){
 
+        $posts = Post::all();
+        return view("blog.index", compact('posts'));
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@
 */
 
 Route::get('/', ['uses' => 'BlogController@index',]);
-});
 
 Route::get('/blog/show', function () {
     return view('blog.show');
