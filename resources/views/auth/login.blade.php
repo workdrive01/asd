e5bcd0d042ac914930793457275c5b4664270575
Jugal <workdrive0110@gmail.com>
2019-07-30 13:11:52 +0545

    using faker added faker data in post and user added new page for blog post made url SEO friendly added post count of users added gravater in user profile added dashboard login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,14 +10,28 @@
         <div class="login-box-body">
             <p class="login-box-msg">Sign in to start your session</p>
 
-            <form action="../../index2.html" method="post">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
                 <div class="form-group has-feedback">
-                    <input type="email" class="form-control" placeholder="Email">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                           name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     <span class="fa fa-envelope form-control-feedback"></span>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                 </div>
                 <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                           name="password" required autocomplete="current-password">
                     <span class="fa fa-lock form-control-feedback"></span>
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                 </div>
                 <div class="row">
                     <div class="col-xs-8">
@@ -36,7 +50,7 @@
             </form>
 
             <br>
-            <a href="#">I forgot my password</a><br>
+            <a href="{{ route('password.request') }}">I forgot my password</a><br>
 
         </div>
         <!-- /.login-box-body -->
